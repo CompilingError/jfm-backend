c93b065178a20b103c1ad5ac2e3b2773e1e37b58
CompilingError <127805783+CompilingError@users.noreply.github.com>
2026-01-06 23:05:08 -0600

    JFM - Create Project

---
+++ src/main/java/com/px/jfmbackend/JfmBackendApplication.java
@@ -0,0 +1,12 @@
+package com.px.jfmbackend;
+
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class JfmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfmBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/px/jfmbackend/JfmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.px.jfmbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfmBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
