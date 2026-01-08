26c26b4f336da5aec0b8ef51b0b09376bf7fbd6c
CompilingError <127805783+CompilingError@users.noreply.github.com>
2026-01-07 22:47:14 -0600

    JFM-1004 - Change DB dependency to SQLite #5

--- src/main/java/com/px/jfmbackend/JfmBackendApplication.java
+++ src/main/java/com/px/jfmbackend/JfmBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@EnableMongoAuditing
 @SpringBootApplication
 public class JfmBackendApplication {
 
