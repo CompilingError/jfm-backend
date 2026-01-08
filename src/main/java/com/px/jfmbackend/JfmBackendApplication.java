72f38dbee186eee2e88498e993eb6e3b75e3918c
CompilingError <127805783+CompilingError@users.noreply.github.com>
2026-01-07 22:48:15 -0600

    JFM-1004 - Delete unused import #5

--- src/main/java/com/px/jfmbackend/JfmBackendApplication.java
+++ src/main/java/com/px/jfmbackend/JfmBackendApplication.java
@@ -3,7 +3,6 @@ package com.px.jfmbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
 public class JfmBackendApplication {
